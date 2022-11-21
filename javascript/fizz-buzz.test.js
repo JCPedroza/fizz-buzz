bf01d09c4e416928acc1d08f454873a0fdf5931b
JCPedroza <nemesis4go10@hotmail.com>
2022-11-21 04:31:28 -0600

    dir: change name

---
+++ javascript/fizz-buzz.test.js
@@ -0,0 +1,68 @@
+const fizzBuzz = require('./fizz-buzz')
+
+/**
+ * Returns an object used to compare two arrays.
+ * @param {number[]} arr Array to compare.
+ * @returns {object} An object used to compare two arrays..
+ */
+const expect = (arr) => ({
+  /**
+   * Checks for deep quality between two arrays.
+   * @param {number[]} expArr Expected array to compare.
+   * @throws Will throw error if arrays are not deeply equal.
+   */
+  toEqual: (expArr) => {
+    // Arrays of different sizes are not equal
+    if (arr.length !== expArr.length) {
+      const msg = `\nArrays of different sizes:\n[${arr}]\n[${expArr}]`
+      throw new Error(msg)
+    }
+
+    // Arrays with different values are not equal
+    for (let index = 0; index < expArr.length; index++) {
+      if (arr[index] !== expArr[index]) {
+        const msg = `\nInequality at index ${index}:\n[${arr}]\n[${expArr}]`
+        throw new Error(msg)
+      }
+    }
+  }
+})
+
+// Inconsistent ranges evaluate to empty arrays
+expect(fizzBuzz(10, 0))
+  .toEqual([])
+
+// One-number ranges evaluate to singleton arrays
+expect(fizzBuzz(0, 0))
+  .toEqual(['fizzbuzz'])
+
+// Positive range
+expect(fizzBuzz(10, 20))
+  .toEqual([
+    'buzz',
+    '11',
+    'fizz',
+    '13',
+    '14',
+    'fizzbuzz',
+    '16',
+    '17',
+    'fizz',
+    '19',
+    'buzz'])
+
+// Negative range
+expect(fizzBuzz(-20, -10))
+  .toEqual([
+    'buzz',
+    '-19',
+    'fizz',
+    '-17',
+    '-16',
+    'fizzbuzz',
+    '-14',
+    '-13',
+    'fizz',
+    '-11',
+    'buzz'
+  ])
